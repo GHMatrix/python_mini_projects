81ef4193ea476abf5a0b461e6e8f8c4085ad3dfa
GHMatrix <gsmensah@yahoo.com>
2025-11-26 15:39:53 +0000

    Todo_list project_menu

--- todo_list.py
+++ todo_list.py
@@ -10,3 +10,11 @@ try:
 		#File does not exist yet
 
 		pass
+
+def show_menu():
+	print("\n --- TO-DO LIST ---")
+	print("1. View Tasks")
+	print("2. Add Task")
+	print("3. Delete Task")
+	print("4. Mark Task As Done")
+	print("5. Exit")
